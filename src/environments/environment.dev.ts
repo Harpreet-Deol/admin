bb920b43931bb52d1cbaec9bec73045965a07cd1
Harpreet Deol <harpreetdeol@Harpreets-MacBook-Pro.local>
2021-08-28 13:43:16 +0530

    first commit

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  baseUrl: 'http://localhost:9000/api', // <Your API base url>
+  imageUrl: '', // <Your API url for image resize>
+  productUrl: '', // <Your store base url>
+  pluginUrl: '' //<Your API url>
+};
